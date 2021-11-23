9b29c9c977aac0a116ede09acc1248e8a10e06c5
Oscar Palomino <oscarpalomino2@gmail.com>
2021-11-23 01:26:52 -0600

    started crud functionality on videos

--- springbootexample/src/main/java/project/controller/AdminController.java
+++ springbootexample/src/main/java/project/controller/AdminController.java
@@ -1,12 +1,19 @@
 package project.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import project.database.dao.TutorialDAO;
+import project.database.entity.Tutorial;
 
 @Controller
 @PreAuthorize("hasAuthority('ADMIN')")
@@ -15,12 +22,104 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminController {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private TutorialDAO tutorialDao;
 
 	@RequestMapping("/protected")
 	public ModelAndView adminPage() {
 		LOG.debug("in /protected");
 		ModelAndView response = new ModelAndView();
 		response.setViewName("admin/protected");
+		return response;
+	}
+	
+	@RequestMapping(value="/videos", method=RequestMethod.GET)
+	public ModelAndView videosPage() {
+		LOG.debug("in /videos");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("admin/videos");
+		
+		//get videos
+		List<Tutorial> videos = tutorialDao.findAll();
+		response.addObject("videos", videos);
+		
+		return response;
+	}
+
+	//create a new video
+	@RequestMapping(value="/videos/new", method=RequestMethod.GET)
+	public ModelAndView newVideoPage() {
+		LOG.debug("in /videos/new");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("admin/newVideo");
+		return response;
+	}
+
+	//save a new video
+	@RequestMapping(value="/videos/new", method=RequestMethod.POST)
+	public ModelAndView newVideo(Tutorial video) {
+		LOG.debug("in /videos/new POST");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("admin/newVideo");
+		
+		//save video
+		tutorialDao.save(video);
+		
+		return response;
+	}
+
+	//edit a video
+	@RequestMapping(value="/videos/edit", method=RequestMethod.GET)
+	public ModelAndView editVideoPage(int id) {
+		LOG.debug("in /videos/edit");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("admin/editVideo");
+		
+		//get video
+		Tutorial video = tutorialDao.findById(id);
+		response.addObject("video", video);
+		
+		return response;
+	}
+
+	//save an edited video
+	@RequestMapping(value="/videos/edit", method=RequestMethod.POST)
+	public ModelAndView editVideo(Tutorial video) {
+		LOG.debug("in /videos/edit POST");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("admin/editVideo");
+		
+		//save video
+		tutorialDao.save(video);
+		
+		return response;
+	}
+
+	//delete a video
+//	@RequestMapping(value="/videos/delete", method=RequestMethod.GET)
+//	public ModelAndView deleteVideo(int id) {
+//		LOG.debug("in /videos/delete");
+//		ModelAndView response = new ModelAndView();
+//		response.setViewName("admin/deleteVideo");
+//		
+//		//get video
+//		Tutorial video = tutorialDao.findById(id);
+//		response.addObject("video", video);
+//		
+//		return response;
+//	}
+
+	//delete a video
+	@RequestMapping(value="/videos/delete", method=RequestMethod.POST)
+	public ModelAndView deleteVideo(Tutorial video) {
+		LOG.debug("in /videos/delete POST");
+		ModelAndView response = new ModelAndView();
+		response.setViewName("redirect:admin/videos");
+		
+		//delete video
+		tutorialDao.delete(video);
+		
 		return response;
 	}
 
---
+++ springbootexample/src/main/webapp/WEB-INF/jsp/admin/videos.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<jsp:include page="../navbar.jsp" />
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Admin</title>
+</head>
+<body>
+	<h1>Videos Page</h1>
+
+	<div class="container">
+		<div class="d-inline p-2">Videos List</div>
+		<div class="d-inline p-2">
+			<button class="btn btn-primary" type="submit" method="GET" formaction="/videos/new">Add Video</button>
+		</div>
+		<hr>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th scope="col">Id</th>
+					<th scope="col">Name</th>
+					<th scope="col">Url</th>
+					<th scope="col">Edit</th>
+					<th scope="col">Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach items="${videos}" var="video">
+					<tr>
+						<td>${video.getId()}</td>
+						<td>${video.getName()}</td>
+						<td>${video.getUrl()}</td>
+						<td>
+							<form method="GET">
+								<input type="hidden" name="id" value="${video.getId()}">
+								<button class="btn btn-success" type="submit"
+									formaction="/videos/edit">Edit</button>
+							</form>
+						</td>
+						<td>
+							<form method="POST" action="/videos/delete">
+								<input type="hidden" name="video" value="${video}">
+								<input type="hidden" name="_method" value="DELETE">
+								<button type="submit" class="btn btn-danger">Delete</button>
+							</form>
+						</td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
